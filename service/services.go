3370b709c0a8610b8cc949e9e1e24a938925ce73
Bright Olawale <oolatunjibright@yahoo.com>
2023-08-04 14:58:25 +0100

    feat: Get books by ID

--- service/services.go
+++ service/services.go
@@ -1,1 +1,28 @@
 package service
+
+import (
+	"errors"
+	"github.com/BrightOlawale/Inventory-API/models"
+)
+
+// Create variable to store item data
+var storage []models.Item = []models.Item{}
+
+// GetAllItems : get all items from the storage.
+func GetAllItems() []models.Item {
+	return storage
+}
+
+// GetItemById : get the item’s data by ID.
+func GetItemById(id string) (models.Item, error) {
+	// We iterate thru all the items in storage
+	for _, item := range storage {
+		// If the current item's ID is exactly same as the id parameter
+		if id == item.ID {
+			// Return item's data
+			return item, nil
+		}
+	}
+	// If item was not found return error
+	return models.Item{}, errors.New("item was not found")
+}

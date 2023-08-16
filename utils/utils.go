065dd03766104751fb22a11abb2ce44a9479545b
Bright Olawale <oolatunjibright@yahoo.com>
2023-08-16 23:36:29 +0100

    feat: Utils function get env variables

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,21 @@
 package utils
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+// GetValue returns the configuration value for the given key from the environment variables
+func GetValue(key string) string {
+	// Load the .env file
+	err := godotenv.Load(".env")
+
+	// If error occurred while loading the .env file
+	if err != nil {
+		log.Fatalf("Error loading .env file\n")
+	}
+
+	// Return the value for the given key
+	return os.Getenv(key)
+}
